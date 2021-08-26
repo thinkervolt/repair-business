6502ff6d3fc91457a67b3bb651e8b31b3c779e68
rustedchip <rustedchip@gmail.com>
2021-08-26 11:10:37 -0700

    install-bug-fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Schema;
+use DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,14 +26,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (Schema::hasTable('notifications')){
-       
-            $notifications = \App\Notification::take(3)->get(); 
-            $notifications_count = \App\Notification::count(); 
-            
-            View::share('notifications', $notifications);
-            View::share('notifications_count', $notifications_count);
-    
+        if(getenv('DB_CONNECTION') AND getenv('DB_DATABASE')){
+            if (Schema::hasTable('notifications')){
+        
+                $notifications = \App\Notification::take(3)->get(); 
+                $notifications_count = \App\Notification::count(); 
+                
+                View::share('notifications', $notifications);
+                View::share('notifications_count', $notifications_count);
+        
             }
+        }
     }
 }
