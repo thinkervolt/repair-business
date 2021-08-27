171a5d6c5db4676632bb2a56b40239753c45e565
rustedchip <rustedchip@gmail.com>
2021-08-26 20:44:06 -0700

    inventory-models-and-migrations

---
+++ database/migrations/2021_08_26_142528_create_inventory_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoryProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('category_id');
+            $table->string('barcode');
+            $table->integer('min_stock')->nullable();
+            $table->integer('max_stock')->nullable();
+            $table->string('email_alert')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory_products');
+    }
+}
---
+++ database/migrations/2021_08_26_142541_create_inventory_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoryTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('transaction');
+            $table->integer('product_id');
+            $table->decimal('purchase_price', 9, 2)->nullable();
+            $table->decimal('selling_price',  9, 2)->nullable();
+            $table->integer('quantity');
+            $table->string('supplier')->nullable();
+            $table->integer('invoice_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory_transactions');
+    }
+}
