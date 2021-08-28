abc395af9058a64324c0db2aa9f8df8fbacdd742
rustedchip <rustedchip@gmail.com>
2021-08-27 19:42:33 -0700

    Ticket #1 - Unknown Database - AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,16 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(getenv('DB_CONNECTION') AND getenv('DB_DATABASE')){
-            if (Schema::hasTable('notifications')){
-        
-                $notifications = \App\Notification::take(3)->get(); 
-                $notifications_count = \App\Notification::count(); 
-                
-                View::share('notifications', $notifications);
-                View::share('notifications_count', $notifications_count);
-        
-            }
-        }
+
     }
 }
