11a46b7f51e2c72eb85ccd6a1c4da8a0b8cb9e2a
rustedchip <rustedchip@gmail.com>
2021-08-29 21:28:12 -0700

    Ticket #1 - Notification bug Fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,23 +7,8 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function __construct()
-    {
-
-        if(getenv('DB_CONNECTION') AND getenv('DB_DATABASE')){
-            if (Schema::hasTable('notifications')){
-                $notifications = \App\Notification::take(3)->get(); 
-                $notifications_count = \App\Notification::count(); 
-                View::share('notifications', $notifications);
-                View::share('notifications_count', $notifications_count);
-            }
-        }
-
-    }
-
-
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Schema;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +26,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        $db_connection = DB::connection()->getPdo();
+        if($db_connection ){
+            if (Schema::hasTable('notifications')){
+                $notifications = \App\Notification::take(3)->get(); 
+                $notifications_count = \App\Notification::count(); 
+                View::share('notifications', $notifications);
+                View::share('notifications_count', $notifications_count);
+            }
+        } 
     }
 }
