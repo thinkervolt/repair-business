2fa0f914308f09921b6efd6745ec5821efb3531e
rustedchip <rustedchip@gmail.com>
2021-09-22 18:16:32 -0700

    Update DataBasePlan.php

--- app/Console/Commands/DataBasePlan.php
+++ app/Console/Commands/DataBasePlan.php
@@ -39,14 +39,17 @@ class DataBasePlan extends Command
      */
     public function handle()
     {
-        $database_plan_path = storage_path()."/app/database-plan";
+        $database_plan_path = "/app/database-plan/";
+    
 
-        if(!Storage::exists($database_plan_path)){
-            Storage::makeDirectory($database_plan_path);
+        if(!Storage::exists("database-plan")){
+            Storage::makeDirectory("database-plan");
         }
 
         $filename = "database-plan-" . Carbon::now()->format('Y-m-d') . ".gz";
-        $command = "mysqldump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | gzip > " . $database_plan_path ."/". $filename;
+
+        echo storage_path()  . $database_plan_path . $filename;
+        $command = "mysqldump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | gzip > ". storage_path()  . $database_plan_path . $filename;
         $returnVar = NULL;
         $output  = NULL;
 
