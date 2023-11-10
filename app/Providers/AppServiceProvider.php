675994e63db668342c791b29f0603028df59b9f6
rustedchip <rustedchip@gmail.com>
2023-11-09 23:27:08 -0800

    app :: proxy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
-
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
