66bcd6806421d414e80f5daa4b1dfad6ca75a3ff
rustedchip <rustedchip@gmail.com>
2024-07-07 07:18:30 +0000

    locale :: spanish

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,3 @@ COPY cloud-run-files/.env.cloud-run /var/www/.env
 RUN chmod 777 -R /var/www/storage/ && \
     chown -R www-data:www-data /var/www/ && \
     a2enmod rewrite 
-#
---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => '¡Hemos enviado por correo electrónico el enlace para restablecer tu contraseña!',
+    'throttled' => 'Por favor, espera antes de intentar de nuevo.',
+    'token' => 'Este token de restablecimiento de contraseña es inválido.',
+    'user' => "No podemos encontrar un usuario con esa dirección de correo electrónico.",
+
+];
