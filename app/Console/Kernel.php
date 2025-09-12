ed0afb3ec9bd06af8673741905f148b4c98d125f
rustedchip <rustedchip@gmail.com>
2025-09-12 06:26:01 +0000

    database-backup-removal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'App\Console\Commands\DataBasePlan'
+        
     ];
 
     /**
@@ -23,8 +23,7 @@ class Kernel extends ConsoleKernel
      * @return void
      */
     protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('database:plan')->daily();
+    { 
     }
 
     /**
